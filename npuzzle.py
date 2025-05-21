dd068bbb909f4e2c1a93fa9685e15881fb7504eb
Nadezhda Zhuzhleva <nzhuzhle@car9s6.42barcelona.com>
2025-05-22 01:04:54 +0200

    options done

--- npuzzle.py
+++ npuzzle.py
@@ -9,7 +9,21 @@ RES = "\033[0m"
 BOLD = "\033[1m"
 MINP = 3
 MAXP = 9
-MSG_PZL_SIZE = "{GREEN}Choose the size of the puzzle (from {MIN} to {MAX}):  {RES}"
+MSG_PZL_SIZE = f"{GREEN}Choose the size of the puzzle (from {MINP} to {MAXP}):  {RES}"
+MSG_ALGO = f"{BLUE}1. A*\n2.Uniform-cost\n3.Greedy search{RES}\n{GREEN}Choose the algorithm:  {RES}"
+MSG_HEURISTIC = f"{BLUE}1. Manhattan-distance\n2. Euclidian distance\n3. ...{RES}\n{GREEN}Choose the heuristic:  {RES}"
+
+algorithms = {
+    1: "A*",
+    2: "Uniform-cost",
+    3: "Greedy search"
+}
+
+heuristics = {
+    1: "Manhattan-distance",
+    2: "Euclidian distance",
+    3: "..."
+}
 
 # sys.argv[0] is the script name
 # sys.argv[1], [2], ... are the actual arguments
@@ -33,21 +47,21 @@ def generate_field(size):
         )
     return mytupplefield
 
-def choose_number(msg):
+def choose_number(msg, minval, maxval):
     try:
-        user_size = input(f"{GREEN}Choose the size of the puzzle (from {MIN} to {MAX}):  {RES}")
+        user_size = input(msg)
         size = int(user_size)
-        if MIN <= size <= MAX:
+        if minval <= size <= maxval:
             return size
         else:
             raise ValueError
     except ValueError:
-        print(f"{RED}Please enter a value between {MIN} and {MAX} !!!{RES}")
-        return choose_number()
+        print(f"{RED}Error: invalid value. Introduce value between {minval} and {maxval} {RES}\n")
+        return choose_number(msg, minval, maxval)
 
 def main(args):
     if len(args) == 1:
-        size = choose_number("{GREEN}Choose the size of the puzzle (from {MIN} to {MAX}):  {RES}")
+        size = choose_number(MSG_PZL_SIZE, MINP, MAXP)
         field = generate_field(size) # IT SHOULD BE MAYBE AN OBJECT (CLASS)???
     elif len(args) == 2:
         field = parse_field(args[1])
@@ -55,11 +69,11 @@ def main(args):
         print(f"{RED}Too many arguments")
         sys.exit(1)
 
-    try:
-        number = input(f"{BLUE}1. A*\n2.Uniform-cost\n3.Greedy search\nChoose the algorithm: ")
+    algo = choose_number(MSG_ALGO, 1, 3)
+    heuristic = choose_number(MSG_HEURISTIC, 1, 3)
 
         
-    print(size)
+    print(f"\n{BLUE}Chosen algorithm: {algorithms[algo]}, chosen heristic: {heuristics[heuristic]}{RES}")
     # sys.exit(1)
 
 # name = sys.argv[1]
